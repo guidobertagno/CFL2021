7bb848d59204ea5a815553d34f5df1b534bf87aa
Guido Bertagno <gbertagno38@gmail.com>
2021-07-07 18:00:28 -0300

    Ejercicio fuera de clase

---
+++ 3. POO/ejercicio 1/cliColor.js
@@ -0,0 +1,5 @@
+let clc = require("cli-color");
+
+console.log(clc.red.bgWhite.underline("Texto en rojo con un fondo blanco"));
+
+//Puedo ejecutarla con node en la consola, pero al asociarla me tira error
